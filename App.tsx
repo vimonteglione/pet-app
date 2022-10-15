64187c93cd47a7b7bcfc8790af145e50034607c1
vimonteglione <vimonteglione@hotmail.com>
2022-10-14 21:15:46 -0300

    Commit de Teste

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+import Card from './src/Card'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Aplicativo</Text>
+      <StatusBar style="dark" />
+      <ScrollView>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +22,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+    padding: 20,
+    marginBottom: 40,
+    paddingTop: 80,
   },
 });
---
+++ src/Card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Card() {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.title}>Curso X</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 50,
+    backgroundColor: "red",
+    marginVertical: 15,
+  },
+  title: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 20,
+  },
+});
